1efc2e203faca25512dde47d576bb1b75b4b225e
mohamed <mohamedhamedcxz@gmail.com>
2024-08-20 02:53:55 +0300

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "//my-portfolio1/",
+  base: '/my-portfolio1/',  // تأكد من أن المسار يبدأ بـ "/"
   plugins: [react()],
 })
-
